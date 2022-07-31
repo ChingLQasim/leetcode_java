220b065d33b641aa9f2122301c09d9707acf1b45
ChingQasim <2608305521@qq.com>
2022-07-31 23:24:17 +0800

    update:回文字符串

---
+++ src/AboutPalindrome/tp680.java
@@ -0,0 +1,24 @@
+package AboutPalindrome;
+
+public class tp680 {
+    int left = 0, right = 0;
+    public boolean validPalindrome(String s) {
+        if (judPali(s, 0, s.length() - 1)) return true;
+        else {
+            int tmpl = left, tmpr = right;
+            return judPali(s, left + 1, right) || judPali(s, tmpl, tmpr - 1);
+        }
+    }
+
+    public boolean judPali(String s, int l, int r){
+        if (s.length() == 1) return true;
+        while(l < r){
+            if (s.charAt(l) != s.charAt(r)) {
+                left = l;right = r;
+                return false;
+            }
+            l++;r--;
+        }
+        return true;
+    }
+}
